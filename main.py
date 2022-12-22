931d85750ffb31a072a1bd6f6e225d3ac4172033
waheeb123 <84958066+waheeb123@users.noreply.github.com>
2022-12-22 18:22:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+# This project is where you show off your ability to (1) translate a business requirement into a database design, (2) design a database using one-to-many and many-to-many relationships, and (3) know when to use LEFT and/or RIGHT JOINs to build result sets for reportingAn organization grants key-card access to rooms based on groups that key-card holders belong to. You may assume that users below to only one group. Your job is to design the database that supports the key-card system.
+There are six users, and four groups. Modesto and Ayine are in group “I.T.” Christopher and Cheong woo are in group “Sales”. There are four rooms: “101”, “102”, “Auditorium A”, and “Auditorium B”. Saulat is in group “Administration.” Group “Operations” currently doesn’t have any users assigned. I.T. should be able to access Rooms 101 and 102. Sales should be able to access Rooms 102 and Auditorium A. Administration does not have access to any rooms. Heidy is a new employee, who has not yet been assigned to any group.
+After you determine the tables any relationships between the tables (One to many? Many to one? Many to many?), you should create the tables and populate them with the information indicated above.
+
+
+
+
+
 DROP TABLE IF EXISTS Users;
 DROP TABLE IF EXISTS Groupss;
 DROP TABLE IF EXISTS Rooms;
@@ -36,3 +44,32 @@ INSERT INTO UsersGroupsRooms ( UgrId, UserId, GroupId, RoomId ) VALUES ( 2101, 1
 INSERT INTO UsersGroupsRooms ( UgrId, UserId, GroupId, RoomId ) VALUES ( 2102, 2, 21, 102) ;
 INSERT INTO UsersGroupsRooms ( UgrId, UserId, GroupId, RoomId ) VALUES ( 2103, 3, 22, 102 );
 INSERT INTO UsersGroupsRooms ( UgrId, UserId, GroupId, RoomId ) VALUES ( 2104, 4, 22, 103) ;
+
+
+--------
+Next, write SELECT statements that provide the following information:
+• All groups, and the users in each group. A group should appear even if there are no users assigned to the group.
+• All rooms, and the groups assigned to each room. The rooms should appear even if no groups have been
+assigned to them.
+• A list of users, the groups that they belong to, and the rooms to which they are assigned. This should be sorted
+alphabetically by user, then by group, then by room.
+
+MySQLSelect =
+ “ SELECT .GroupName, u. UserName FROM Users u INNER JOIN UsersGroups ug
+ON u. UserId = ug. UserId RIGHT JOIN Groupss g
+ON ug.GroupId = g.GroupId;
+SELECT r.RoomName, g.GroupName FROM Groupss g INNER JOIN GroupsRooms
+ON g. GroupId
+= gr.GroupId RIGHT JOIN Rooms r
+ON gr. RoomId = r.RoomId;
+SELECT u.UserName, g.GroupName, r.RoomName
+FROM Users
+u INNER JOIN UsersGroupsRooms ugr
+ON u.UserId =
+ugr. UserId INNER JOIN Groupss
+ON ugr. GroupId
+= g. GroupId INNER JOIN Rooms
+r
+ON ugr.RoomId = r.RoomId
+ORDER BY UserName, GroupName, RoomName; “
+
